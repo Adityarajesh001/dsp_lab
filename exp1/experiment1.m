260c34870a123c6b5bbe7e78070448b47553f823
Adityarajesh001 <119749228+Adityarajesh001@users.noreply.github.com>
2023-08-14 20:05:51 +0530

    Update experiment1.m

--- exp1/experiment1.m
+++ exp1/experiment1.m
@@ -62,16 +62,38 @@ xlabel('Time')
 ylabel('Audio Signal')
 
 %4th
-[y,Fs] = audioread('speech.wav');
-t=linspace(0,length(y)/Fs,length(y));
-Nfft=16777216; %power of 2 and I put a huge number so there are many data point
-f=linspace(0,Fs,Nfft);
-X1=abs(fft(y,Nfft));
-figure(2);
-plot(f(1:Nfft/2),X1(1:Nfft/2))
-xlabel('Frequency'); 
-ylabel ('amp');
-title ('FFT Spectrum');
+F = 250.*a;
+data = importdata('speech.wav');
+[s,Fs] = audioread('speech.wav');
+y1 = fn(s, F, Fs);
+T = 1/Fs;       
+L = 1500;
+t = (0:L-1)*T;
+y0 = fft(y1);
+s0 = fft(s);
+f = Fs*(0:(L/2))/L;
+P2 = abs(y0/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+Q2 = abs(s0/L);
+Q1 = Q2(1:L/2+1);
+Q1(2:end-1) = 2*Q1(2:end-1);
+subplot(2,2,1);
+plot(y1);
+xlabel('time');
+ylabel('y');
+subplot(2,2,2);
+plot(s);
+xlabel('time');
+ylabel('s');
+subplot(2,2,3);
+plot(f, P1,'blue', f, Q1,'red');%peaks with different magnitudes but occuring at same frequency
+xlabel('frequency');
+ylabel('Amplitude');
+legend('y1','s')
+function y = fn(s, F, Fs)
+y = s.*cos(2.*pi.*F./Fs);
+end
 
 
 %5th
